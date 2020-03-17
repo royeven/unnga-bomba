3fe2ad7423c6163dadd214dd8064b53c58c5f5a1
royeven <36963512+royeven@users.noreply.github.com>
2020-03-17 16:24:30 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onButtonPressed(Button.B, function () {
+	
+})
+input.onButtonPressed(Button.A, function () {
+	
+})
+let bombe = game.createSprite(Math.randomRange(0, 4), 0)
+let spiller = game.createSprite(2, 4)
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
