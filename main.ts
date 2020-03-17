8ecf73c7ab52485401640276e26138054106ace9
royeven <36963512+royeven@users.noreply.github.com>
2020-03-17 16:25:18 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
 input.onButtonPressed(Button.B, function () {
-	
+    spiller.change(LedSpriteProperty.X, 1)
 })
 input.onButtonPressed(Button.A, function () {
-	
+    spiller.change(LedSpriteProperty.X, -1)
 })
+let spiller: game.LedSprite = null
 let bombe = game.createSprite(Math.randomRange(0, 4), 0)
-let spiller = game.createSprite(2, 4)
+spiller = game.createSprite(2, 4)
 basic.forever(function () {
 	
 })
