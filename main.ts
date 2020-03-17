54e1b40881a3a669196aa3ec20f03ccbe4e4a05f
royeven <36963512+royeven@users.noreply.github.com>
2020-03-17 16:26:14 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,6 +7,9 @@ input.onButtonPressed(Button.A, function () {
 let spiller: game.LedSprite = null
 let bombe = game.createSprite(Math.randomRange(0, 4), 0)
 spiller = game.createSprite(2, 4)
+game.setLife(3)
+game.setScore(0)
+let pause2 = 1000
 basic.forever(function () {
 	
 })
