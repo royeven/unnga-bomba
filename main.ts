9816825404c8f93e277bcedfbb5464f19c56fa7a
royeven <36963512+royeven@users.noreply.github.com>
2020-03-17 16:27:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,7 +11,13 @@ game.setLife(3)
 game.setScore(0)
 let pause2 = 1000
 basic.forever(function () {
-	
+    bombe.change(LedSpriteProperty.Y, 1)
+    basic.pause(pause2)
+    if (bombe.get(LedSpriteProperty.Y) == 4) {
+        bombe.set(LedSpriteProperty.Y, 0)
+        bombe.set(LedSpriteProperty.X, Math.randomRange(0, 4))
+        basic.pause(pause2)
+    }
 })
 basic.forever(function () {
 	
